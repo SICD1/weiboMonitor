9105fccd2aadf8515fe528e065580b24686cb68e
ws <289204469@qq.com>
2016-09-08 09:05:26 +0800

    Rejected commit: Default

--- src/com/sicdlib/dto/RolePrivilege.java
+++ src/com/sicdlib/dto/RolePrivilege.java
@@ -1,34 +1,11 @@
 package com.sicdlib.dto;
 
-import com.sicdlib.entity.*;
 import com.sicdlib.entity.Role;
 
-import java.util.HashSet;
-import java.util.Set;
-
-/**
- * Created by maninit on 2016/9/7.
- */
 public class RolePrivilege {
-    private String rp_id;
-    private Set<Role> roles = new HashSet<Role>();
-    private Set<Privilege> privileges=new HashSet<Privilege>();
+    private String rp_id;private Role role;
+    private Privilege privilege;
 
-    public Set<Privilege> getPrivileges() {
-        return privileges;
-    }
-
-    public void setPrivileges(Set<Privilege> privileges) {
-        this.privileges = privileges;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 
     public String getRp_id() {
         return rp_id;
@@ -38,4 +15,19 @@ public class RolePrivilege {
         this.rp_id = rp_id;
     }
 
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Privilege getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(Privilege privilege) {
+        this.privilege = privilege;
+    }
 }
